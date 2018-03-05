127ed38fd16e098635504b97fdbdcec030bf83dd
Lukáš Karabina <xkarab04@vutbr.cz>
2018-03-05 20:22:47 +0100

    Feature: Photo Class. Jsou v tom metody pro klíč, název, čas pořízení, formát a rozlišení. Formáty jsem enumem vybral ty používané.

---
+++ PhotoGallery.DAL/Photo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.DAL
+{
+    public class Photo
+    {
+        public Guid IdKey { get; set; } = Guid.NewGuid();
+
+
+        public string PhotoName { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public Format Format { get; set; }
+
+        public Resolution Resolution { get; set; }
+    }
+}
