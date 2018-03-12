d26db223389788f579eb5d573ac28fb1ded9e8c1
Dominik Konečný <xkonec65@vutbr.cz>
2018-03-12 11:38:26 +0100

    added tag abstract class

---
+++ PhotoGallery.DAL/tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.DAL
+{
+    abstract class Tag
+    {
+
+
+        Tag(float xPos, float yPos)
+        {
+            positionOnPhotox = xPos;
+            positionOnPhotoy = yPos;
+        }
+
+        public float positionOnPhotox { get; set; }
+        public float positionOnPhotoy { get; set; }
+
+    }
+}
