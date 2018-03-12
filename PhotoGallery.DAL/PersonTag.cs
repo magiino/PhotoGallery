f931ec8c38d421bcc45562960372c7f845152034
Dominik Konečný <xkonec65@vutbr.cz>
2018-03-12 12:35:58 +0100

    přidáno ItemTag a PersonTag, pár úprav v Tag

---
+++ PhotoGallery.DAL/PersonTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.DAL
+{
+    class PersonTag : Tag
+    {
+        public string PersonName { get; private set; }
+        public string PersonSurname { get; private set; }
+
+        PersonTag(string name, string surname, int xPos, int yPos)
+        {
+            PersonName = name;
+            PersonSurname = surname;
+            PositionOnPhotoX = xPos;
+            PositionOnPhotoY = yPos;
+        }
+
+    }
+}
