b50c3475af7f40afa21d27bddc3341c44248eb3a
Lukáš Karabina <xkarab04@vutbr.cz>
2018-04-09 23:23:31 +0200

    Location changed back to string

--- PhotoGallery.BL/Models/PhotoDetailModel.cs
+++ PhotoGallery.BL/Models/PhotoDetailModel.cs
@@ -16,8 +16,7 @@ namespace PhotoGallery.BL.Models
         public int ResolutionId { get; set; }
         public ResolutionEntity Resolution { get; set; }
         public string Note { get; set; }
-        public int LocationId { get; set; }
-        public LocationEntity Location { get; set; }
+        public string Location { get; set; }
         public ICollection<AlbumEntity> Albums { get; set; }
         public ICollection<TagEntity> Tags { get; set; }
     }
