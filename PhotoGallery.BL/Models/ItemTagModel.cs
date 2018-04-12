9747547f5f1ea70006bb99eff410023a44b5ba38
Dominik Konečný <xkonec65@vutbr.cz>
2018-04-12 16:41:25 +0200

    přidáno PhotoTagModel a ItemTagModel

---
+++ PhotoGallery.BL/Models/ItemTagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using PhotoGallery.DAL.Enums;
+using PhotoGallery.DAL.Entities;
+
+namespace PhotoGallery.BL.Models
+{
+    class ItemTagModel
+    {
+        public string Name { get; set; }
+
+        public int PositionOnPhotoX { get; set; }
+       
+        public int PositionOnPhotoY { get; set; }
+
+        public virtual ICollection<PhotoEntity> PhotosWithThisTag { get; set; } = new List<PhotoEntity>();
+
+    }
+}
---
+++ PhotoGallery.BL/Models/PersonTagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PhotoGallery.DAL.Enums;
+using PhotoGallery.DAL.Entities;
+namespace PhotoGallery.BL.Models
+{
+    class PersonTagModel
+    {
+        public int PersonId { get; set; }
+
+        public PersonEntity Person { get; set; }
+
+        public int PositionOnPhotoX { get; set; }
+
+        public int PositionOnPhotoY { get; set; }
+
+        public virtual ICollection<PhotoEntity> PhotosWithThisTag { get; set; } = new List<PhotoEntity>();
+
+    }
+}
