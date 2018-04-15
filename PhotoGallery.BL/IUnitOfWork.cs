e2f73cf02e765c676ec6d6060213203e4909b451
Marek Magáth <xmagat01@vutbr.cz>
2018-04-16 00:59:24 +0200

    repositories defined in iunitofwork

--- PhotoGallery.BL/IUnitOfWork.cs
+++ PhotoGallery.BL/IUnitOfWork.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using PhotoGallery.BL.Repositories.Interfaces;
 
 namespace PhotoGallery.BL
 {
     public interface IUnitOfWork : IDisposable
     {
+        IAlbumRepository Albums { get; }
+        IPhotoRepository Photos { get; }
+        IItemTagRepository ItemTags { get; }
+        IPersonTagRepository PersonTags { get; }
+
         int Complete();
     }
 }
