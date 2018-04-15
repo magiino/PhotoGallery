3e73a3a095fda432ef589d2bd6055110aecce503
Marek Magáth <xmagat01@vutbr.cz>
2018-04-16 00:59:24 +0200

    repositories defined in iunitofwork

--- PhotoGallery.BL/UnitOfWork.cs
+++ PhotoGallery.BL/UnitOfWork.cs
@@ -1,16 +1,22 @@
-﻿using PhotoGallery.DAL;
+﻿using PhotoGallery.BL.Repositories.Interfaces;
+using PhotoGallery.DAL;
 
 namespace PhotoGallery.BL
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly DataContext _dataContext;
+        public IAlbumRepository Albums => throw new System.NotImplementedException();
+        public IPhotoRepository Photos => throw new System.NotImplementedException();
+        public IItemTagRepository ItemTags => throw new System.NotImplementedException();
+        public IPersonTagRepository PersonTags => throw new System.NotImplementedException();
 
         public UnitOfWork(DataContext context)
         {
             _dataContext = context;
 
         }
+
         public int Complete()
         {
             return _dataContext.SaveChanges();
